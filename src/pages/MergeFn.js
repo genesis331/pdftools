3934a9310425c28600df6d41806de343b8b8a566
genesis331 <cheah3838@gmail.com>
2023-03-02 23:15:48 +0800

    init page contents with new components

--- src/pages/MergeFn.js
+++ src/pages/MergeFn.js
@@ -1,44 +1,72 @@
-import {Group, Text, useMantineTheme} from "@mantine/core";
-import {Dropzone, MIME_TYPES} from "@mantine/dropzone";
-import {IconFile, IconUpload, IconX} from "@tabler/icons";
+import { useState } from "react";
+import { Button, Container, Flex, Group, Loader, Stepper, Text, useMantineTheme } from "@mantine/core";
+import { Dropzone, MIME_TYPES } from "@mantine/dropzone";
+import { IconDownload, IconFile, IconUpload, IconX } from "@tabler/icons";
 
 export default function MergeFn() {
     const theme = useMantineTheme();
+    const [active, setActive] = useState(0);
+    const nextStep = () => setActive((current) => (current < 3 ? current + 1 : current));
 
-    return <div>
-        <Dropzone
-            onDrop={(files) => console.log('accepted files', files)}
-            onReject={(files) => console.log('rejected files', files)}
-            maxSize={3 * 1024 ** 2}
-            accept={[MIME_TYPES.pdf]}
-        >
-            <Group position="center" spacing="xl" style={{ minHeight: 220, pointerEvents: 'none' }}>
-                <Dropzone.Accept>
-                    <IconUpload
-                        size={50}
-                        stroke={1.5}
-                        color={theme.colors[theme.primaryColor][theme.colorScheme === 'dark' ? 4 : 6]}
-                    />
-                </Dropzone.Accept>
-                <Dropzone.Reject>
-                    <IconX
-                        size={50}
-                        stroke={1.5}
-                        color={theme.colors.red[theme.colorScheme === 'dark' ? 4 : 6]}
-                    />
-                </Dropzone.Reject>
-                <Dropzone.Idle>
-                    <IconFile size={50} stroke={1.5} />
-                </Dropzone.Idle>
-                <div>
-                    <Text size="xl" inline>
-                        Drag PDFs here or click to select PDFs
+    function handleFileDrop(files) {
+        console.log(files);
+        nextStep();
+    }
+
+    return <Container>
+        <Stepper active={active} onStepClick={setActive} breakpoint="sm" allowNextStepsSelect={false}>
+            <Stepper.Step label="Upload file" pt={15} mb={20} pl={10}>
+                <Dropzone
+                    onDrop={(files) => handleFileDrop(files)}
+                    accept={[MIME_TYPES.pdf]}
+                >
+                    <Group position="center" spacing="xl" style={{ minHeight: 220, pointerEvents: 'none' }}>
+                        <Dropzone.Accept>
+                            <IconUpload
+                                size={50}
+                                stroke={1.5}
+                                color={theme.colors[theme.primaryColor][theme.colorScheme === 'dark' ? 4 : 6]}
+                            />
+                        </Dropzone.Accept>
+                        <Dropzone.Reject>
+                            <IconX
+                                size={50}
+                                stroke={1.5}
+                                color={theme.colors.red[theme.colorScheme === 'dark' ? 4 : 6]}
+                            />
+                        </Dropzone.Reject>
+                        <Dropzone.Idle>
+                            <IconFile size={50} stroke={1.5} />
+                        </Dropzone.Idle>
+                        <div>
+                            <Text size="xl" inline>
+                                Drag PDFs here or click to select PDFs
+                            </Text>
+                            <Text size="sm" color="dimmed" inline mt={7}>
+                                Attach as many PDFs as you want. The file size should not exceed 5mb.
+                            </Text>
+                        </div>
+                    </Group>
+                </Dropzone>
+            </Stepper.Step>
+            <Stepper.Step label="Merge file" pt={15} mb={20}>
+                <Flex align="center" justify="center" my={20}>
+                    <Loader />
+                    <Text fz="md" ml={20}>
+                        Merging file...
                     </Text>
-                    <Text size="sm" color="dimmed" inline mt={7}>
-                        Attach as many PDFs as you want. The file size should not exceed 5mb.
-                    </Text>
-                </div>
-            </Group>
-        </Dropzone>
-    </div>
+                </Flex>
+            </Stepper.Step>
+            <Stepper.Step label="Download file" pt={15} mb={20} pr={10}>
+                <Text fw={500} fz="md" align="center">
+                    Done merging file.
+                </Text>
+                <Flex justify="center" py={15}>
+                    <Button leftIcon={<IconDownload size="1rem" />}>
+                        Download
+                    </Button>
+                </Flex>
+            </Stepper.Step>
+        </Stepper>
+    </Container>
 }
